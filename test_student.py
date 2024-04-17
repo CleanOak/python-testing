8aefe89df2771ad92219c5de7a70203758d51dbf
Morgan Asare <ohenemorgan@gmail.com>
2024-04-17 17:06:52 +0000

    Add mocking test for course schedule success and failed

--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
 import unittest
 from student import Student
 from datetime import date, timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase): 
@@ -24,7 +25,7 @@ class TestStudent(unittest.TestCase):
 
     def test_full_name(self):
         print("test_full_name")
-        
+
         self.assertEqual(self.student.full_name, 'John Doe')
     
 
@@ -49,8 +50,20 @@ class TestStudent(unittest.TestCase):
         self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
 
     
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
 
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
 
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+        
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
 
 
 
