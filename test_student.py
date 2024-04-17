0dc43677c583f1e606a7b1a415999bd519d655f0
Morgan Asare <ohenemorgan@gmail.com>
2024-04-17 16:51:12 +0000

    Add apply_extension test finction

--- test_student.py
+++ test_student.py
@@ -1,5 +1,6 @@
 import unittest
 from student import Student
+from datetime import date, timedelta
 
 
 class TestStudent(unittest.TestCase): 
@@ -23,18 +24,29 @@ class TestStudent(unittest.TestCase):
 
     def test_full_name(self):
         print("test_full_name")
+        
         self.assertEqual(self.student.full_name, 'John Doe')
     
 
     def test_email(self):
         print("test_email")
+
         self.assertEqual(self.student.email, 'john.doe@email.com')
     
 
     def test_alert_santa(self):
         print("test_alert_santa")
         self.student.alert_santa()
+
         self.assertTrue(self.student.naughty_list)
+
+    
+    def test_apply_extension(self):
+        print('test_apply_extensioin')
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
 
     
 
